adfa74ae695b8de5a918140288f015431727b837
Malinda Ratnaduhita <malindard@students.unnes.ac.id>
2025-06-27 15:22:39 +0700

    Tambah fitur Product

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PhishingController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return redirect('/product');
+});
+
 Route::get('/phishing', [PhishingController::class, 'index']);
 Route::post('/phishing/check', [PhishingController::class, 'check']);
+Route::get('/product', [ProductController::class, 'index']);
