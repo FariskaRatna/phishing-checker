c362e35ea8fee3d220f2ecb8177803f2e03dc11c
Panda L540 <abdihimam@gmail.com>
2025-07-21 22:49:15 +0700

    Menghilangkan tombol close dari popup result URl

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -303,7 +303,6 @@
 
             <div class="modal-header">
                 <h5 class="modal-title" id="resultModalLabel">Hasil Analisis</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
 
             <div class="modal-body" id="modalResultContent">
